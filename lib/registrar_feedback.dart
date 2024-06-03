7f6be86b239dccd93cc0a718ed4fa437d4ef5426
adrianaugis123 <adrianaugis@yahoo.com>
2024-06-03 12:36:21 +0800

    add: thank you user & sorry user pages

--- lib/registrar_feedback.dart
+++ lib/registrar_feedback.dart
@@ -1,5 +1,8 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_rating_bar/flutter_rating_bar.dart';
+import 'package:registrar_log_feedback_app/sorry_user.dart';
+import 'package:registrar_log_feedback_app/thank_you_user.dart';
 
 class RegistrarFeedback extends StatefulWidget {
   String staffName;
@@ -11,6 +14,7 @@ class RegistrarFeedback extends StatefulWidget {
 }
 
 class _RegistrarFeedbackState extends State<RegistrarFeedback> {
+  final TextEditingController _emailController = TextEditingController();
   String _name = '';
   String _email = '';
   String _feedback = '';
@@ -19,6 +23,20 @@ class _RegistrarFeedbackState extends State<RegistrarFeedback> {
   int _window = 0;
   double _rating = 0.0;
   final _formKey = GlobalKey<FormState>();
+
+  String? _validateEmail(String? value) {
+    if (value == null || value.isEmpty) {
+      return 'Please enter your Email';
+    }
+    final RegExp emailRegExp = RegExp(
+      r'^[a-zA-Z0-9._%-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$',
+    );
+    if (!emailRegExp.hasMatch(value)) {
+      return 'Please enter a valid Email';
+    }
+    return null;
+  }
+
   @override
   Widget build(BuildContext context) {
     _staffName = widget.staffName;
@@ -79,22 +97,15 @@ class _RegistrarFeedbackState extends State<RegistrarFeedback> {
                 },
               ),
               TextFormField(
+                controller: _emailController,
                 decoration: const InputDecoration(labelText: 'Email'),
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Please enter your email';
-                  }
-                  return null;
-                },
-                onSaved: (value) {
-                  _email = value!;
-                },
+                validator: _validateEmail,
               ),
               TextFormField(
                 decoration: const InputDecoration(labelText: 'Remarks'),
                 validator: (value) {
                   if (value == null || value.isEmpty) {
-                    return 'Please enter your feedback';
+                    return 'Please enter your Remarks';
                   }
                   return null;
                 },
@@ -106,9 +117,19 @@ class _RegistrarFeedbackState extends State<RegistrarFeedback> {
               const SizedBox(height: 16),
               Center(child: ElevatedButton(
                 onPressed: () {
-                  if (_formKey.currentState!.validate()) {
+                  if (_formKey.currentState!.validate() && _window != 3) {
                     _formKey.currentState!.save();
+                    Navigator.pushReplacement(
+                      context,
+                      MaterialPageRoute(builder: (context) => const ThankYouUser()),
+                    );
                     // Process the feedback (e.g., send it to a server)
+                  }else if (_window == 3){
+                    _formKey.currentState!.save();
+                    Navigator.pushReplacement(
+                      context,
+                      MaterialPageRoute(builder: (context) => const SorryUser()),
+                    );
                   }
                 },
                 child: const Text('Submit'),
